6e72f3c8058fe8b5ab738fb29aaec4232772086f
Eric <eric.vacquet06@gmail.com>
2021-07-09 17:56:40 +0200

    Modification du code JS et adaptation du code HTML afin de faire apparaître les différents cadres

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,24 +1,51 @@
-fetch("http://localhost:3000/api/teddies")
-.then(response => {
-    return response.json()
-})
-.then(teddies => {
-    teddies.forEach(teddy => {
-        document.getElementById("image__teddies").innerHTML += teddy.imageUrl + "<br>";
+// Main function
+;(async() => {
+    const teddies = await getTeddies()
+    garbagePage(teddies)
+})()
+
+
+
+
+// On appelle l'API avec un fetch
+
+async function getTeddies() {
+    return fetch("http://localhost:3000/api/teddies")
+    .then(response => response.json())
+    
+    .then((teddies) => teddies)
+    .catch((error) => {
+        alert("La connexion au serveur n'a pas pu être établie")
     })
-})
-
-
-/* function displayArticle(article) {
-    const templateElt = document.getElementById("template__article")
-    const cloneElt = document.importNode(templateElt.content, true)
-
-    cloneElt.getElementById("name__teddies").textContent = section.name__teddies
-    cloneElt.getElementById("id__teddies").textContent = section.id__teddies
-    cloneElt.getElementById("description__teddies").textContent = section.description__teddies
-    cloneElt.getElementById("color__teddies").textContent = section.color__teddies
+    
 }
 
-// Colors / id / name / imageURL / description /
-//    5   / 3  /  2   /    1     /      4      /
-*/
+function garbagePage(teddies) {
+    document.getElementById("articleList").innerHTML = ''
+
+// On ajoute une boucle pour afficher les éléments 
+    teddies.forEach((teddy) => {
+        displayTeddies(teddy)
+    })
+}
+
+
+function displayTeddies(teddy) {
+    // On add le template
+    const templateElt = document.getElementById("product")
+
+    // On clone le template
+    const cloneElt = document.importNode(templateElt.content, true)
+
+    // On defini chaque élément
+    cloneElt.getElementById("image__teddies").src = teddy.imageUrl
+    cloneElt.getElementById("name__teddies").textContent = teddy.name
+    cloneElt.getElementById("price__teddies").textContent = `${teddy.price / 100}.00 €`
+
+    // On affiche le template
+    document.getElementById("articleList").appendChild(cloneElt)
+}
+
+
+
+
